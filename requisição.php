239c6eed695f50ff52d381b25dc37d65343673bb
davi ribeiro <luminuszz43@gmail.com>
2019-05-24 12:10:16 -0300

    first commit

---
+++ requisição.php
@@ -0,0 +1,107 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+</head>
+
+<body>
+    <div class="card container" style="margin-bottom: 200px; margin-top:200px;">
+        <div class="card-header">
+            <h5 class="card-title">Formulario de requisição de serviço</h5>
+        </div>
+        <div class="card-body">
+            <div class="form-group">
+                <label for="fornecdor">Grupo de usuário</label>
+                <select class="form-control" name="fornecdor" id="fornecdor">
+                    <option>dsa</option>
+                    <option>dsad</option>
+                    <option>sad</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="Servico">Serviço:</label>
+                <select class="form-control" name="Servico" id="Servico">
+                    <option>dsa</option>
+                    <option>dsad</option>
+                    <option>sad</option>
+                </select>
+            </div>
+
+            <div class="form-group">
+                <label for="assunto">Assunto:</label>
+                <select class="form-control" name="assunto" id="assunto">
+                    <option>dsa</option>
+                    <option>dsad</option>
+                    <option>sad</option>
+                </select>
+            </div>
+
+            <div class="form-group">
+                <label for="mensagem">Mensagem</label>
+                <textarea class="form-control" name="mensagem" id="mensagem" rows="3">algo aqui</textarea>
+            </div>
+                <div class="form-group">
+                  <label for="data">Data Encerramento</label>
+                  <input type="date" name="data" id="data" class="form-control" placeholder="data" aria-describedby="helpId">
+                </div>
+                 <div class="form-group">
+                   <label for="arquivos">Arquivos</label>
+                   <input type="file" class="form-control-file btn btn-outline-primary" name="arquivos" id="arquivos" placeholder="arquivos" aria-describedby="fileHelpId">
+                 </div>
+                    </div>
+
+                    <div class="container">
+                        <h5 class="card-title">Produtos necessarios</h5>
+                    
+                             <div class="row">
+                        
+                             <div class="col">col</div>
+                             <div class="w-100"></div>
+                             <div class="col">col</div>
+                                 <div class="w-100"></div>
+                             <div class="col">col</div>
+                             
+                             <div class="col">col</div>
+                             
+                             <div class="col">col</div>
+                             
+                             <div class="col">col</div>
+                             <div class="w-100"></div>
+
+                            </div>
+                          
+
+                    
+                        
+                    </div>
+
+                </div>
+
+            
+
+
+    </div>
+
+
+
+
+
+
+
+
+    <script src="js/bootstrap-datepicker.min.js"></script>
+ <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+
+</html>
